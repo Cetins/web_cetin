e6b630450952ec2186a78a645a03659a504d11fe
Cetins <69481095+Cetins@users.noreply.github.com>
2023-04-12 12:13:34 +0100

    global.css imported, layout component added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@components/styles/globals.css'
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
+  return getLayout(<Component {...pageProps} />);
 }
+
+export default App;
