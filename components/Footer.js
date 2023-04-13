67d667dedc0edcd93334e4dcc85807f05e587748
Cetins <69481095+Cetins@users.noreply.github.com>
2023-04-13 09:47:44 +0100

    footer media queries added

--- components/Footer.js
+++ components/Footer.js
@@ -5,12 +5,12 @@ import styles from '../styles/Footer.module.css';
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2023 Next.js Example</p>
-      <p>cetinsdev@gmail.com</p>
+      <p className={styles.sm_p}>&copy; 2023 Next.js Example</p>
+      <p className={styles.sm_p}>cetinsdev@gmail.com</p>
       <span className={styles.social}>
-        <button><a href='https://github.com/Cetins'><AiFillGithub className={styles.footerIcons}/></a></button>
-        <button><a href='https://www.linkedin.com/in/cetins/'><AiFillLinkedin className={styles.footerIcons}/></a></button>
-        <button><a href='https://medium.com/@cetins'><FaBlog className={styles.footerIcons}/></a></button>
+        <button className={styles.social_button}><a href='https://github.com/Cetins'><AiFillGithub className={styles.footerIcons}/></a></button>
+        <button className={styles.social_button}><a href='https://www.linkedin.com/in/cetins/'><AiFillLinkedin className={styles.footerIcons}/></a></button>
+        <button className={styles.social_button}><a href='https://medium.com/@cetins'><FaBlog className={styles.footerIcons}/></a></button>
       </span>
     </footer>
   );
